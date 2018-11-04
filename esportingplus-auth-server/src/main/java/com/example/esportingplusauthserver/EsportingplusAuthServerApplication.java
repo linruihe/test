ef62e211a108baa17fa95e668651f310cd3e7d23
linruihe <linruihe007@163.com>
2018-11-04 10:30:37 +0800

    提交项目

---
+++ esportingplus-auth-server/src/main/java/com/example/esportingplusauthserver/EsportingplusAuthServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.esportingplusauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportingplusAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsportingplusAuthServerApplication.class, args);
+    }
+}
